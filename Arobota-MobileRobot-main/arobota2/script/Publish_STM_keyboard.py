20b60920ccaeb2d24c6ce467a7c7d93581a0cfa2
Fin <chanun3571@gmail.com>
2022-10-08 01:16:46 -0700

    keyboard

---
+++ Arobota-MobileRobot-main/arobota2/script/Publish_STM_keyboard.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import serial
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String, Float32
+
+ser = serial.Serial ("/dev/ttyUSB1", 115200) #Open port with baud rate 
+rospy.Subscriber("/key", String, self.keyCallback, queue_size=1)
+
+class STM_Connect():
+    def __init__(self): 
+        rospy.init_node('STM_Pub',anonymous=True)
+        self._left_wheel_speed_ = 0
+        self._right_wheel_speed_ = 0
+        self._center_wheel_speed_ = 0
+        self.wheel_speed = []
+        self.wheel = 0
+        rospy.loginfo("Publish data to STM")
+
+    def keyCallback(self, msg):
+        
+        self.wheel = msg.data.split(',')
+        #M0 = power command 
+        #M1 = position command
+        #M2 = velocity command
+        u_x =
+        u_y = 
+        speed_message = "M0"+"A"+str(int(float(self.wheel[0])*5000))+"B"+str(int(float(self.wheel[1])*5000))+"C"+str(int(float(self.wheel[2])*5000))+"\r\n"
+        ser.write(bytes(speed_message, 'utf-8'))
+        
+if __name__ =='__main__':
+	try:
+		STM_Connect() 	
+		rospy.spin()
+	except rospy.ROSInterruptException:
+		rospy.logwarn("Connection Failed")
