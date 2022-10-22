12e0830d16879271d81a3d0e46db1c72287c114c
Fin <chanun3571@gmail.com>
2022-10-22 03:36:05 -0700

    update

--- main/arobota2/script/Publish_STM_keyboard.py
+++ main/arobota2/script/Publish_STM_keyboard.py
@@ -4,7 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String, Float32
 
-ser = serial.Serial ("/dev/ttyUSB1", 115200) #Open port with baud rate 
+ser = serial.Serial("/dev/ttyUSB1", 115200) # type: ignore #Open port with baud rate 
 
 class STM_Connect():
     def __init__(self): 
