32b11b9918b9d313e6272625d94baadfa661c1e0
Fin <chanun3571@gmail.com>
2022-11-02 04:18:36 -0700

    go

--- main/arobota2/script/Joystickteleop.py
+++ main/arobota2/script/Joystickteleop.py
@@ -23,20 +23,20 @@ class Joystick_Input():
         joy_omega = msg.axes[XBoxButton.RX]
         self._uh.linear.x = joy_ux #(-1,1)
         self._uh.linear.y = joy_uy #(-1,1)
-        self._uh.angular.z = joy_omega #(-1,1)
+        self._uh.angular.z = joy_omega*5 #(-1,1)
 
     def motion(self,w,vx,vy):
         rate = rospy.Rate(10)
-        r = (48/2)/1000 #m
+        r = (48/2)/1000 # m
         d = 100/1000 #m
         #print(w,vx,vy)
         u1 = 1/r*(-d*w + vx)
         u2 = 1/r*(-d*w -cos(pi/3)*vx -sin(pi/3)*vy)
         u3 = 1/r*(-d*w -cos(pi/3)*vx + sin(pi/3)*vy)
         ros_translation = Vector3()
-        ros_translation.x = u1
-        ros_translation.y = u2
-        ros_translation.z = u3
+        ros_translation.x = u1 
+        ros_translation.y = u2 
+        ros_translation.z = u3 
         self.pub.publish(ros_translation)
         rospy.loginfo(ros_translation)
         rate.sleep()
@@ -44,9 +44,9 @@ class Joystick_Input():
     def spin(self):
         # initialize message
         while not rospy.is_shutdown():
-            rate = rospy.Rate(100)
+            #rate = rospy.Rate(100)
             self.motion(self._uh.angular.z,self._uh.linear.x,self._uh.linear.y)
-            rate.sleep()
+            #rate.sleep()
             
 
 if __name__=='__main__':
