831dc2b0df32226b77a4cc000387aa0a168d38ee
Fin <chanun3571@gmail.com>
2022-11-02 00:33:18 -0700

    up

--- main/arobota2/script/Publish_STM_joystick.py
+++ main/arobota2/script/Publish_STM_joystick.py
@@ -23,15 +23,15 @@ class STM_Connect():
         #M0 = power command 
         #M1 = position command
         #M2 = velocity command
-        self._left_wheel_power = msg.x * 5
-        self._center_wheel_power = msg.y * 5
-        self._right_wheel_power= msg.z *5
+        self._left_wheel_power = msg.x 
+        self._center_wheel_power = msg.y 
+        self._right_wheel_power= msg.z
         #power command   
-        #speed_message = "M0"+"A"+str(int(self._right_wheel_power))+"B"+str(self._center_wheel_power)+"C"+str(self._left_wheel_power)+"\r\n"
-        #ser.write(bytes(speed_message, 'utf-8'))
+        power_message = "M0"+"A"+str(int(self._right_wheel_power))+"B"+str(self._center_wheel_power)+"C"+str(self._left_wheel_power)+"\r\n"
+        ser.write(bytes(power_message, 'utf-8'))
         #velocity command
-        velocity_message = "M2"+"A"+str(int(self._right_wheel_power))+"B"+str(self._center_wheel_power)+"C"+str(self._left_wheel_power)+"\r\n"
-        ser.write(bytes(velocity_message, 'utf-8'))
+        #velocity_message = "M2"+"A"+str(int(self._right_wheel_power))+"B"+str(self._center_wheel_power)+"C"+str(self._left_wheel_power)+"\r\n"
+        #ser.write(bytes(velocity_message, 'utf-8'))
         #rospy.loginfo(speed_message)
         r.sleep()
         #velocity command
