95cc194be61d1b7dfc34136c73a3c655247bd610
Fin <chanun3571@gmail.com>
2023-03-17 12:57:47 +0700

    robot does not move fix

--- main/arobota2/script/publish_STM_omni.py
+++ main/arobota2/script/publish_STM_omni.py
@@ -18,7 +18,7 @@ class STM_Connect():
     def Update_Speed(self, msg):
         r = rospy.Rate(20)
         self.wheel = msg.data.split(',')
-        self.left_wheel_power = int(float(self.wheel[0]))*100
+        self.left_wheel_power = int(float(self.wheel[0]))*200
         self.center_wheel_power = int(float(self.wheel[1]))*200
         self.right_wheel_power = int(float(self.wheel[2]))*200
         # print ("M2A"+str(int(float(self.wheel[0])*1000000))+"B"+str(int(float(self.wheel[1])*1000000))+"\r\n")
