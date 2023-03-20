5517fd9bc79d00e933659289f549faac5805f0e1
Fin <chanun3571@gmail.com>
2023-03-20 17:49:13 +0700

    powera

--- main/arobota2/script/publish_STM_omni.py
+++ main/arobota2/script/publish_STM_omni.py
@@ -22,17 +22,17 @@ class STM_Connect():
         self.right_wheel_power = -int(float(self.wheel[2])*100)
         #dead zone
         if 0<abs(self.left_wheel_power)<10 and 0<abs(self.right_wheel_power)<10 and 0<abs(self.center_wheel_power)<10:
-            if self.left_wheel_powera<0:
+            if self.left_wheel_power<0:
                 self.left_wheel_power = self.left_wheel_power - 10
-            if self.left_wheel_powera<0:
+            if self.left_wheel_power<0:
                 self.right_wheel_power = self.right_wheel_power -10 
-            if self.center_wheel_powera<0:
+            if self.center_wheel_power<0:
                 self.center_wheel_power = self.center_wheel_power -10 
-            if self.left_wheel_powera>0:
+            if self.left_wheel_power>0:
                 self.left_wheel_power = self.left_wheel_power + 10
-            if self.left_wheel_powera>0:
+            if self.left_wheel_power>0:
                 self.right_wheel_power = self.right_wheel_power +10 
-            if self.center_wheel_powera>0:
+            if self.center_wheel_power>0:
                 self.center_wheel_power = self.center_wheel_power +10 
                
         # print ("M2A"+str(int(float(self.wheel[0])*1000000))+"B"+str(int(float(self.wheel[1])*1000000))+"\r\n")
