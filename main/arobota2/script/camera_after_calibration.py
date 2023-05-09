c55f496b49e3c33ad7c9ca4a261fd2574cb355c4
Fin <chanun3571@gmail.com>
2023-05-09 18:59:41 +0700

    calibrate cam

---
+++ main/arobota2/script/camera_after_calibration.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+
+
+# Camera parameters to undistort and rectify images
+cv_file = cv2.FileStorage()
+cv_file.open('stereoMap.xml', cv2.FileStorage_READ)
+
+stereoMapL_x = cv_file.getNode('stereoMapL_x').mat()
+stereoMapL_y = cv_file.getNode('stereoMapL_y').mat()
+stereoMapR_x = cv_file.getNode('stereoMapR_x').mat()
+stereoMapR_y = cv_file.getNode('stereoMapR_y').mat()
+
+
+# Open both cameras
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+
+
+while cap.isOpened():
+    success, img = cap.read()
+    # img = cv2.flip(img,0)
+    # right = img[:360,0:640]
+    # left = img[:360,640:]
+
+
+    # Undistort and rectify images
+    # right = cv2.remap(right, stereoMapR_x, stereoMapR_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+    # left = cv2.remap(left, stereoMapL_x, stereoMapL_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+    img = cv2.remap(img, stereoMapL_x, stereoMapL_y, cv2.INTER_LANCZOS4, cv2.BORDER_CONSTANT, 0)
+         
+    # Show the frames
+    # cv2.imshow("left cam", left)
+    # cv2.imshow("right cam", right) 
+    cv2.imshow("right cam", img) 
+
+
+    # Hit "q" to close the window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# Release and destroy all windows before termination
+cap.release()
+
+cv2.destroyAllWindows()
---
+++ main/arobota2/script/camera_generate_chessboard.py
@@ -0,0 +1,34 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 2560)
+# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+num = 0
+
+while cap.isOpened():
+
+    succes1, img = cap.read()
+    img = cv2.flip(img,0)
+    # left = img[:720,0:1280]
+    # right = img[:720,1280:]
+    right = img[:360,0:640]
+    left = img[:360,640:]
+    k = cv2.waitKey(5)
+
+    if k == 27:
+        break
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite('images/stereoLeft/imageL' + str(num) + '.png', left)
+        cv2.imwrite('images/stereoRight/imageR' + str(num) + '.png', right)
+        print("images saved!")
+        num += 1
+
+    cv2.imshow('left cam',left)
+    cv2.imshow('right cam',right)
+
+# Release and destroy all windows before termination
+cap.release()
+cv2.destroyAllWindows()
