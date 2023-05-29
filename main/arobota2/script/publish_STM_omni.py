9b79f50a74484325710253888f67fe6925cffa9e
central <chanun3571@gmail.com>
2023-05-29 14:40:21 +0700

    push

--- main/arobota2/script/publish_STM_omni.py
+++ main/arobota2/script/publish_STM_omni.py
@@ -15,21 +15,21 @@ class STM_Connect():
         self.center_wheel_speed_ = 0     
         rospy.loginfo("Publish data to STM")
         rospy.Subscriber('wheel_vtarget',String,self.Update_Speed)
-        rospy.Subscriber('/joystick', Vector3, self.joystickCallback)
+    #     rospy.Subscriber('/joystick', Vector3, self.joystickCallback)
 
-    def joystickCallback(self, msg):
-        r = rospy.Rate(20)
-        #M0 = power command 
-        #M1 = position command
-        #M2 = velocity command
-        self._left_wheel_power = -int(msg.z) 
-        self._center_wheel_power = -int(msg.x)
-        self._right_wheel_power= -int(msg.y)
-        #power command   
-        power_message = "M0"+"A"+str(self._left_wheel_power)+"B"+str(self._right_wheel_power)+"C"+str(self._center_wheel_power)+"\r\n"
-        ser.write(bytes(power_message, 'utf-8'))
-        rospy.loginfo(power_message)
-        r.sleep()
+    # def joystickCallback(self, msg):
+    #     r = rospy.Rate(20)
+    #     #M0 = power command 
+    #     #M1 = position command
+    #     #M2 = velocity command
+    #     self._left_wheel_power = -int(msg.z) 
+    #     self._center_wheel_power = -int(msg.x)
+    #     self._right_wheel_power= -int(msg.y)
+    #     #power command   
+    #     power_message = "M0"+"A"+str(self._left_wheel_power)+"B"+str(self._right_wheel_power)+"C"+str(self._center_wheel_power)+"\r\n"
+    #     ser.write(bytes(power_message, 'utf-8'))
+    #     rospy.loginfo(power_message)
+    #     r.sleep()
         
     def Update_Speed(self, msg):
         r = rospy.Rate(10)
