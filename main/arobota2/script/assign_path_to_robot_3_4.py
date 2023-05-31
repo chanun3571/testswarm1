a2d72affafc6f8e45b8f09f8982dcfcd2f74c1bd
central <chanun3571@gmail.com>
2023-05-31 11:11:04 +0700

    push

--- main/arobota2/script/assign_path_to_robot_3_4.py
+++ main/arobota2/script/assign_path_to_robot_3_4.py
@@ -5,17 +5,17 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionResul
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray,Pose,Quaternion,Twist,Point
 from std_msgs.msg import Int32,String
 
-class publish_goal_pose_to_robot1():
+class publish_goal_pose_to_robot3():
     def __init__(self):
-        rospy.init_node('custom_waypoints1')
-        rospy.loginfo('start robot1')
-        rospy.Subscriber('/robot1_formation_pos', Pose, self.waypoint)
-        rospy.Subscriber('/robot1/move_base/result',MoveBaseActionResult,self.failcallback1)
-        self.flag = rospy.Publisher('/robot1/flag', String, queue_size=1)
+        rospy.init_node('custom_waypoints3')
+        rospy.loginfo('start robot3')
+        rospy.Subscriber('/robot3_formation_pos', Pose, self.waypoint)
+        rospy.Subscriber('/robot3/move_base/result',MoveBaseActionResult,self.failcallback3)
+        self.flag = rospy.Publisher('/robot3/flag', String, queue_size=1)
         rospy.Subscriber('/swarm1/done', String, self.donecallback)
         self.done = "WAIT"
         self.locations = dict()
-        self.flag1 = 0
+        self.flag3 = 0
         self.ready = False
     def donecallback(self,msg):
         self.done = msg.data
@@ -23,15 +23,14 @@ class publish_goal_pose_to_robot1():
 
     def waypoint(self, msg):
         self.pose = msg
-        self.ready=True
-
+        self.ready = True
+        
     def sendGoals(self, waypoint):
         # subscribe to action server 
-        client = actionlib.SimpleActionClient('robot1/move_base', MoveBaseAction)
+        client = actionlib.SimpleActionClient('robot3/move_base', MoveBaseAction)
         # this command to wait for the server to start listening for goals.
         client.wait_for_server()
         if self.ready:
-            self.reached = False
             goal = MoveBaseGoal()
             goal.target_pose.header.frame_id = "map"
             goal.target_pose.header.stamp = rospy.Time.now()
@@ -42,7 +41,7 @@ class publish_goal_pose_to_robot1():
                 if wait and self.reached:
                     self.flag_done = "1"
                     self.flag.publish(self.flag_done)
-                    # rospy.loginfo("robot1 done")
+                    # rospy.loginfo("robot3 done")
                     self.done = self.done
                 if self.done == "DONE":
                     self.flag_done = "0"
@@ -51,37 +50,36 @@ class publish_goal_pose_to_robot1():
                     break
 
 
-
-    def failcallback1(self, msg):
+    def failcallback3(self, msg):
         # if msg.status.text=="Failed to find a valid plan. Even after executing recovery behaviors.":
         rospy.loginfo(msg.status.text)
         if msg.status.text=="Robot is oscillating. Even after executing recovery behaviors." or \
            msg.status.text=="Failed to find a valid control. Even after executing recovery behaviors." or \
            msg.status.text=="Failed to find a valid plan. Even after executing recovery behaviors." :
-            self.flag1 = 1
+            self.flag3 = 1
             self.sendGoals(self.locations)
-            rospy.loginfo("robot1")
-            print(self.flag1)
+            rospy.loginfo("robot3")
+            print(self.flag3)
         if msg.status.text=="Goal reached.":
             self.reached = True
             
-    def resubmit1(self):
-        if self.flag1 == 1:
-            self.flag1 = 0
+    def resubmit3(self):
+        if self.flag3 == 1:
+            self.flag3 = 0
             self.sendGoals(self.locations)
-            rospy.loginfo("resubmit robot #1")
-            print(self.flag1)
+            rospy.loginfo("resubmit robot #3")
+            print(self.flag3)
 
     def spin(self):
         # initialize message
         while not rospy.is_shutdown():
             self.sendGoals(self.locations)
-            # self.resubmit1()
+            # self.resubmit3()
             rospy.Rate(5).sleep()
 
 if __name__=='__main__':
     try:
-        agent=publish_goal_pose_to_robot1()
+        agent=publish_goal_pose_to_robot3()
         agent.spin()
 
     except rospy.ROSInterruptException:
